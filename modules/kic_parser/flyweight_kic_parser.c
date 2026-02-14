e5daffde47a4fa0cbd82658df6b9ddb4fc85c55d
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-02-15 00:08:25 +0900

    chore(kic_notation): write CMakeLists.txt to describe how to build this project
    Adopt TDD(Test Driven Development).

---
+++ modules/kic_parser/flyweight_kic_parser.c
@@ -0,0 +1,5 @@
+#include "flyweight_kic_parser.h"
+#include <stdio.h>
+
+// INFO: stub!
+void FKP() { printf("At flyweight_kic_parser.c, hello, world\n"); }
---
+++ test/kic_parser/flyweight_kic_parser_test.c
@@ -0,0 +1,7 @@
+#include "kic_parser/flyweight_kic_parser.h"
+
+// INFO: stub!
+int main(void) {
+  FKP();
+  return 0;
+}
