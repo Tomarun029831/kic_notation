95bf875d1ceb8acdb2ca81c11cf4af5a7dd62046
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-02-15 23:31:28 +0900

    feat(modules): implement check_availability_of_kic to pass kic_version_test

--- modules/kic_parser/flyweight_kic_parser.c
+++ modules/kic_parser/flyweight_kic_parser.c
@@ -1,5 +1,25 @@
 #include "flyweight_kic_parser.h"
-#include <stdio.h>
+#include "kic_parser/kic_parser_specifications.h"
 
-// INFO: stub!
-void FKP() { printf("At flyweight_kic_parser.c, hello, world\n"); }
+#define KIC_HEADER "KIC:" KIC_VERSION
+#define KIC_SEPARATOR ';'
+#define KIC_TERMINATOR '/'
+
+unsigned char check_availability_of_kic(const char *string) {
+  const char *kic_header = KIC_HEADER;
+  for (;; string++) {
+    if (*kic_header == ((char)'\0') && *string == KIC_SEPARATOR)
+      break; // pass first test
+    if (*string != *kic_header++)
+      return KIC_UNAVAILABLE;
+  }
+
+  for (;; string++) {
+    if (*string == KIC_TERMINATOR)
+      break; // pass second test
+    if (*string == ((char)'\0'))
+      return KIC_UNAVAILABLE;
+  }
+
+  return KIC_AVAILABLE;
+}
--- modules/kic_parser/flyweight_kic_parser.h
+++ modules/kic_parser/flyweight_kic_parser.h
@@ -2,4 +2,4 @@
 #define KIC_UNAVAILABLE ((unsigned char)0)
 #define KIC_AVAILABLE ((unsigned char)1)
 // check availability of kic with version and syntax
-unsigned char check_availability_of_kic(const char *const string);
+unsigned char check_availability_of_kic(const char *string);
