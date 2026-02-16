7cf7c3a4505aaf8821a9100ae824ebb35b741cf2
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-02-16 15:18:54 +0900

    feat(test): add some test-cases

--- test/kic_parser/flyweight_kic_parser_test.c
+++ test/kic_parser/flyweight_kic_parser_test.c
@@ -4,14 +4,21 @@
 #include <stdio.h>
 
 inline static void kic_version_test() {
-  const char *const kic_correct_version = "KIC:" KIC_VERSION ";/";
-  const char *const kic_incorrect_version = "KIC:V0;/";
-  const unsigned char expected_available =
-      check_availability_of_kic(kic_correct_version);
-  assert(expected_available == KIC_AVAILABLE);
-  const unsigned char expected_unavailable =
-      check_availability_of_kic(kic_incorrect_version);
-  assert(expected_unavailable == KIC_UNAVAILABLE);
+  const char *const kic_correct_version[2] = {
+      "KIC:" KIC_VERSION ";/", "KIC:" KIC_VERSION ";1290;03000300;00900/"};
+  for (const char *const *head = &kic_correct_version[0];
+       head != kic_correct_version + 2; head++) {
+    const unsigned char expected_available = check_availability_of_kic(*head);
+    assert(expected_available == KIC_AVAILABLE);
+  }
+
+  const char *const kic_incorrect_version[4] = {"KIC:V0;/", "AIC:V0;/",
+                                                "KAC:V0;/", "KIA:V0;/"};
+  for (const char *const *head = &kic_incorrect_version[0];
+       head != kic_incorrect_version + 4; head++) {
+    const unsigned char expected_unavailable = check_availability_of_kic(*head);
+    assert(expected_unavailable == KIC_UNAVAILABLE);
+  }
 }
 
 inline static void kic_syntax_test() {}
