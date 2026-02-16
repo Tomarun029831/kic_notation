272c4421e307350d74d8836223b3600eea9ea2f7
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-02-16 15:49:23 +0900

    feat(test, modules): add kic_syntax_test

--- modules/kic_parser/flyweight_kic_parser.h
+++ modules/kic_parser/flyweight_kic_parser.h
@@ -2,3 +2,8 @@
 #define KIC_CONPATIBLE ((unsigned char)0)
 #define KIC_INCONPATIBLE ((unsigned char)1)
 unsigned char check_kic_compatibility(const char *string);
+
+#define KIC_SYNTAX_CORRECT ((unsigned char)0)
+#define KIC_SYNTAX_ERROR ((unsigned char)1)
+// this function will call check_kic_compatibility() with the argument
+unsigned char check_kic_syntax(const char *string);
--- test/kic_parser/flyweight_kic_parser_test.c
+++ test/kic_parser/flyweight_kic_parser_test.c
@@ -8,20 +8,30 @@ inline static void kic_version_test() {
       "KIC:" KIC_VERSION ";/", "KIC:" KIC_VERSION ";1290;03000300;00900/"};
   for (const char *const *head = &kic_correct_version[0];
        head != kic_correct_version + 2; head++) {
-    const unsigned char expected_available = check_kic_compatibility(*head);
-    assert(expected_available == KIC_CONPATIBLE);
+    const unsigned char expected_compatible = check_kic_compatibility(*head);
+    assert(expected_compatible == KIC_CONPATIBLE);
   }
 
   const char *const kic_incorrect_version[4] = {"KIC:V0;/", "AIC:V0;/",
                                                 "KAC:V0;/", "KIA:V0;/"};
   for (const char *const *head = &kic_incorrect_version[0];
        head != kic_incorrect_version + 4; head++) {
-    const unsigned char expected_unavailable = check_kic_compatibility(*head);
-    assert(expected_unavailable == KIC_INCONPATIBLE);
+    const unsigned char expected_incompatible = check_kic_compatibility(*head);
+    assert(expected_incompatible == KIC_INCONPATIBLE);
   }
 }
 
-inline static void kic_syntax_test() {}
+inline static void kic_syntax_test() {
+  const char *kic_correct_syntax =
+      "KIC:" KIC_VERSION ";1290;03000300;009001223;21023/";
+  const unsigned char expected_correct = check_kic_syntax(kic_correct_syntax);
+  assert(expected_correct == KIC_SYNTAX_CORRECT);
+
+  const char *kic_wrong_syntax =
+      "KIC:" KIC_VERSION ";129GNUIS0030NOTUNIX.223;21023/";
+  const unsigned char expected_wrong = check_kic_syntax(kic_wrong_syntax);
+  assert(expected_wrong == KIC_SYNTAX_ERROR);
+}
 
 int main(void) {
   kic_version_test();
