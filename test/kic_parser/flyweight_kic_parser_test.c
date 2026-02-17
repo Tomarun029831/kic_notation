16d131cffe6c47d5765bf63994cad9ae35721012
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-02-17 12:59:57 +0900

    feat(test): enhance kic_syntax_test

--- test/kic_parser/flyweight_kic_parser_test.c
+++ test/kic_parser/flyweight_kic_parser_test.c
@@ -22,30 +22,50 @@ static inline void kic_version_test() {
 }
 
 static inline void kic_syntax_test() {
-  const char *kic_correct_syntax[2] = {
+  const char *kic_correct_syntax[3] = {
       "KIC:" KIC_VERSION ";01200;00010001;/",
-      "KIC:" KIC_VERSION ";01437;01140334;008001200;20700090011001330;/"};
+      "KIC:" KIC_VERSION ";01437;01140334;008001200;20700090011001330;/",
+      "KIC:" KIC_VERSION ";01437;01140334;008001200;10100;20700090011001330;"
+      "390001200;41200;51200;61200;/"};
   for (const char *const *head = &kic_correct_syntax[0];
-       head != kic_correct_syntax + 2; head++) {
+       head != kic_correct_syntax + 3; head++) {
     const unsigned char expected_correct = check_kic_syntax(*head);
     assert(expected_correct == KIC_SYNTAX_CORRECT);
   }
 
-  const char *kic_wrong_syntax[4] = {
+  const char *kic_wrong_syntax[6] = {
       "KIC:" KIC_VERSION ";129GNUIS0030NOTUNIX.223;21023/",
       "KIC:" KIC_VERSION ";9000;90009000;11200;/",
       "KIC:" KIC_VERSION ";01200;0090;11200;/",
-      "KIC:" KIC_VERSION ";01200;00900090;1120012009;/"};
+      "KIC:" KIC_VERSION ";01200;00900090;1120012009;/",
+      "KIC:" KIC_VERSION ";01437;01140334;008001200;10100;20700090011001330;"
+      "390001200;41200;51200;61200;712001200;/",
+      "KIC:" KIC_VERSION ";01437;01140334;008001200;10100;20700090011001330;"
+      "390001200;41200;51200;61200;712001200/"};
   for (const char *const *head = &kic_wrong_syntax[0];
-       head != kic_wrong_syntax + 4; head++) {
+       head != kic_wrong_syntax + 6; head++) {
     const unsigned char expected_wrong = check_kic_syntax(*head);
     assert(expected_wrong == KIC_SYNTAX_ERROR);
   }
 }
 
+static inline void kic_get_timestamp_test() {}
+static inline void kic_get_boardsize_test() {}
+static inline void kic_get_schedules_test() {}
+static inline void kic_get_day_of_week_test() {}
+static inline void kic_get_time_in_schedule_test() {}
+
 int main(void) {
   kic_version_test();
   kic_syntax_test();
+  // below here, these tests do NOT check syntax of array of chars.
+  // So you must let the tests check array of chars which has correct kic syntax
+  kic_get_timestamp_test();
+  kic_get_boardsize_test();
+  kic_get_schedules_test();
+  kic_get_day_of_week_test();
+  kic_get_time_in_schedule_test();
+
   puts("flyweight_kic_parser_test passed");
   return 0;
 }
