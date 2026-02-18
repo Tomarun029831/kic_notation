1fc885d9d9ef04731b3127f96ecc0228486bf2a3
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-02-18 19:40:59 +0900

    refactor(test): replace magic_number with macro to calc length of array and adopt appropriate type

--- test/kic_parser/flyweight_kic_parser_test.c
+++ test/kic_parser/flyweight_kic_parser_test.c
@@ -4,6 +4,8 @@
 #include <stddef.h>
 #include <stdio.h>
 
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
 static void kic_boolean_test_iterator(const char *const *array_to_pass_test,
                                       const size_t len_of_array,
                                       const unsigned char (*func)(const char *),
@@ -16,25 +18,27 @@ static void kic_boolean_test_iterator(const char *const *array_to_pass_test,
 }
 
 static inline void kic_version_test() {
-  const char *const kic_correct_version[2] = {
+  const char *const kic_correct_version[] = {
       "KIC:" KIC_VERSION ";/", "KIC:" KIC_VERSION ";01290;03000300;00900/"};
-  kic_boolean_test_iterator(kic_correct_version, 2, check_kic_compatibility,
-                            KIC_CONPATIBLE);
-  const char *const kic_incorrect_version[4] = {"KIC:V0;/", "AIC:V0;/",
-                                                "KAC:V0;/", "KIA:V0;/"};
-  kic_boolean_test_iterator(kic_incorrect_version, 4, check_kic_compatibility,
-                            KIC_INCONPATIBLE);
+  kic_boolean_test_iterator(kic_correct_version,
+                            ARRAY_SIZE(kic_correct_version),
+                            check_kic_compatibility, KIC_CONPATIBLE);
+  const char *const kic_incorrect_version[] = {"KIC:V0;/", "AIC:V0;/",
+                                               "KAC:V0;/", "KIA:V0;/"};
+  kic_boolean_test_iterator(kic_incorrect_version,
+                            ARRAY_SIZE(kic_incorrect_version),
+                            check_kic_compatibility, KIC_INCONPATIBLE);
 }
 
 static inline void kic_syntax_test() {
-  const char *kic_correct_syntax[3] = {
+  const char *kic_correct_syntax[] = {
       "KIC:" KIC_VERSION ";01200;00010001;/",
       "KIC:" KIC_VERSION ";01437;01140334;008001200;20700090011001330;/",
       "KIC:" KIC_VERSION ";01437;01140334;008001200;10100;20700090011001330;"
       "390001200;41200;51200;61200;/"};
-  kic_boolean_test_iterator(kic_correct_syntax, 3, check_kic_syntax,
-                            KIC_SYNTAX_CORRECT);
-  const char *kic_wrong_syntax[6] = {
+  kic_boolean_test_iterator(kic_correct_syntax, ARRAY_SIZE(kic_correct_syntax),
+                            check_kic_syntax, KIC_SYNTAX_CORRECT);
+  const char *kic_wrong_syntax[] = {
       "KIC:" KIC_VERSION ";129GNUIS0030NOTUNIX.223;21023/",
       "KIC:" KIC_VERSION ";9000;90009000;11200;/",
       "KIC:" KIC_VERSION ";01200;0090;11200;/",
@@ -43,42 +47,43 @@ static inline void kic_syntax_test() {
       "390001200;41200;51200;61200;712001200;/",
       "KIC:" KIC_VERSION ";01437;01140334;008001200;10100;20700090011001330;"
       "390001200;41200;51200;61200;712001200/"};
-  kic_boolean_test_iterator(kic_wrong_syntax, 6, check_kic_syntax,
-                            KIC_SYNTAX_ERROR);
+  kic_boolean_test_iterator(kic_wrong_syntax, ARRAY_SIZE(kic_wrong_syntax),
+                            check_kic_syntax, KIC_SYNTAX_ERROR);
 }
 
 static inline void kic_get_timestamp_test() {
-  const char *kic_correct_syntax[3] = {
+  const char *kic_correct_syntax[] = {
       "KIC:" KIC_VERSION ";01200;00010001;/",
       "KIC:" KIC_VERSION ";11437;01140334;008001200;20700090011001330;/",
       "KIC:" KIC_VERSION ";21437;01140334;008001200;10100;20700090011001330;"
       "390001200;41200;51200;61200;/"};
-  const unsigned int expected_timestamps[3] = {
+  const unsigned int expected_timestamps[] = {
       TIMESTAMP(0, 1200), TIMESTAMP(1, 1437), TIMESTAMP(2, 1437)};
 
   for (const char *const *head = kic_correct_syntax;
-       head != kic_correct_syntax + 3; head++) {
+       head != kic_correct_syntax + ARRAY_SIZE(kic_correct_syntax); head++) {
     unsigned int result = get_kic_timestamp(*head);
-    assert(result == expected_timestamps[(head - kic_correct_syntax)]);
+    ptrdiff_t idx = head - kic_correct_syntax;
+    assert(result == expected_timestamps[idx]);
   }
 }
 
 static inline void kic_get_boardsize_test() {
-  const char *kic_correct_syntax[3] = {
+  const char *kic_correct_syntax[] = {
       "KIC:" KIC_VERSION ";01200;00010001;/",
       "KIC:" KIC_VERSION ";11437;01140334;008001200;20700090011001330;/",
       "KIC:" KIC_VERSION ";21437;01140334;008001200;10100;20700090011001330;"
       "390001200;41200;51200;61200;/"};
-  const BoardSize expected_timestamps[3] = {
-      BOARDSIZE(1, 1), BOARDSIZE(114, 334), BOARDSIZE(114, 334)};
+  const BoardSize expected_boardsizes[] = {BOARDSIZE(1, 1), BOARDSIZE(114, 334),
+                                           BOARDSIZE(114, 334)};
 
   for (const char *const *head = kic_correct_syntax;
-       head != kic_correct_syntax + 3; head++) {
+       head != kic_correct_syntax + ARRAY_SIZE(kic_correct_syntax); head++) {
     const BoardSize result = get_kic_boardsize(*head);
-    assert(result.height_cm ==
-               expected_timestamps[(head - kic_correct_syntax)].height_cm &&
-           result.width_cm ==
-               expected_timestamps[(head - kic_correct_syntax)].width_cm);
+    ptrdiff_t idx = head - kic_correct_syntax;
+    const BoardSize expected_boardsize = expected_boardsizes[(idx)];
+    assert(result.height_cm == expected_boardsize.height_cm &&
+           result.width_cm == expected_boardsize.width_cm);
   }
 }
 
