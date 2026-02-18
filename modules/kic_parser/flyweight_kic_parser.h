5be8a552c6ddb9332ceceaacd0f987909871c5a5
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-02-18 17:33:00 +0900

    feat(modules): implement get_kic_timestamp(renamed find_kic_timestamp) and get_kic_boardsize(renamed find_kic_boardsize) and define new type(BoardSize) to pass test

--- modules/kic_parser/flyweight_kic_parser.h
+++ modules/kic_parser/flyweight_kic_parser.h
@@ -8,15 +8,21 @@ const unsigned char check_kic_compatibility(const char *string);
 // this function will call check_kic_compatibility() with the argument
 const unsigned char check_kic_syntax(const char *string);
 
+#define TIMESTAMP(day, time) ((unsigned int)(day * 10000 + time))
+typedef struct {
+  const unsigned int height_cm;
+  const unsigned int width_cm;
+} BoardSize;
+#define BOARDSIZE(h, w) ((BoardSize){(h), (w)})
 // this function will NOT check the syntax,
 // So you need to call check_kic_syntax(const char *string),
 // if you cannot ensure that the argument of this function follows kic-format
-const char *find_kic_timestamp(const char *string);
+const unsigned int get_kic_timestamp(const char *string);
 
 // this function will NOT check the syntax,
 // So you need to call check_kic_syntax(const char *string),
 // if you cannot ensure that the argument of this function follows kic-format
-const char *find_kic_boardsize(const char *string);
+BoardSize get_kic_boardsize(const char *string);
 
 // this function will NOT check the syntax,
 // So you need to call check_kic_syntax(const char *string),
