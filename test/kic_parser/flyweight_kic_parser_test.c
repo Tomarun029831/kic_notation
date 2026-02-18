ff5b1973147503b287954496ef1faad5c8c0676e
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-02-18 14:15:47 +0900

    refactor(test): rename kic_test_iteration_helper -> kic_boolean_test_iterator

--- test/kic_parser/flyweight_kic_parser_test.c
+++ test/kic_parser/flyweight_kic_parser_test.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 #include <stdio.h>
 
-static void kic_test_iteration_helper(const char *const *array_to_pass_test,
+static void kic_boolean_test_iterator(const char *const *array_to_pass_test,
                                       const int len_of_array,
                                       const unsigned char (*func)(const char *),
                                       const unsigned char expected_result) {
@@ -17,11 +17,11 @@ static void kic_test_iteration_helper(const char *const *array_to_pass_test,
 static inline void kic_version_test() {
   const char *const kic_correct_version[2] = {
       "KIC:" KIC_VERSION ";/", "KIC:" KIC_VERSION ";01290;03000300;00900/"};
-  kic_test_iteration_helper(kic_correct_version, 2, check_kic_compatibility,
+  kic_boolean_test_iterator(kic_correct_version, 2, check_kic_compatibility,
                             KIC_CONPATIBLE);
   const char *const kic_incorrect_version[4] = {"KIC:V0;/", "AIC:V0;/",
                                                 "KAC:V0;/", "KIA:V0;/"};
-  kic_test_iteration_helper(kic_incorrect_version, 4, check_kic_compatibility,
+  kic_boolean_test_iterator(kic_incorrect_version, 4, check_kic_compatibility,
                             KIC_INCONPATIBLE);
 }
 
@@ -31,7 +31,7 @@ static inline void kic_syntax_test() {
       "KIC:" KIC_VERSION ";01437;01140334;008001200;20700090011001330;/",
       "KIC:" KIC_VERSION ";01437;01140334;008001200;10100;20700090011001330;"
       "390001200;41200;51200;61200;/"};
-  kic_test_iteration_helper(kic_correct_syntax, 3, check_kic_syntax,
+  kic_boolean_test_iterator(kic_correct_syntax, 3, check_kic_syntax,
                             KIC_SYNTAX_CORRECT);
   const char *kic_wrong_syntax[6] = {
       "KIC:" KIC_VERSION ";129GNUIS0030NOTUNIX.223;21023/",
@@ -42,7 +42,7 @@ static inline void kic_syntax_test() {
       "390001200;41200;51200;61200;712001200;/",
       "KIC:" KIC_VERSION ";01437;01140334;008001200;10100;20700090011001330;"
       "390001200;41200;51200;61200;712001200/"};
-  kic_test_iteration_helper(kic_wrong_syntax, 6, check_kic_syntax,
+  kic_boolean_test_iterator(kic_wrong_syntax, 6, check_kic_syntax,
                             KIC_SYNTAX_ERROR);
 }
 
