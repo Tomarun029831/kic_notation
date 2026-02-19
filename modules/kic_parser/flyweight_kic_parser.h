51113346006ed8d20595120e2490c3bfe311778b
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-02-19 20:59:24 +0900

    feat(modules, test)!: add two fields to Timestamp(type)

--- modules/kic_parser/flyweight_kic_parser.h
+++ modules/kic_parser/flyweight_kic_parser.h
@@ -12,24 +12,32 @@ const unsigned char check_kic_syntax(const char *string);
 
 typedef union {
   struct {
-    uint16_t time : 11; // 0~1159
-    uint16_t day : 3;   // 0~6
-    uint16_t is_PM : 1; // 0 - am or 1 - pm
-    uint16_t unused : 1;
+    uint32_t millisecond : 10; // 0~999
+    uint32_t second : 6;       // 0~59
+    uint32_t hour_min : 11;    // 0~1159
+    uint32_t day : 3;          // 0~6
+    uint32_t is_PM : 1;        // 0:AM, 1:PM
+    uint32_t unused : 1;
   } segments;
-  uint16_t raw;
+  uint32_t raw;
 } Timestamp;
 #define HALF_DAY_OFFSET (1200)
 static inline Timestamp TIMESTAMP(unsigned int d, unsigned int t) {
   const char is_PM = (t >= HALF_DAY_OFFSET);
-  return (Timestamp){.segments = {.is_PM = is_PM,
+  return (Timestamp){.segments = {.millisecond = 0,
+                                  .second = 0,
+                                  .hour_min = is_PM ? (t - HALF_DAY_OFFSET) : t,
                                   .day = d,
-                                  .time = is_PM ? (t - HALF_DAY_OFFSET) : t,
+                                  .is_PM = is_PM,
                                   .unused = 0}};
 }
 #define TIMESTAMP_RAW(d, t, pm)                                                \
-  ((Timestamp){                                                                \
-      .segments = {.time = (t), .day = (d), .is_PM = (pm), .unused = 0}})
+  ((Timestamp){.segments = {.millisecond = 0,                                  \
+                            .second = 0,                                       \
+                            .hour_min = (t),                                   \
+                            .day = (d),                                        \
+                            .is_PM = (pm),                                     \
+                            .unused = 0}})
 // this function will NOT check the syntax,
 // So you need to call check_kic_syntax(const char *string),
 // if you cannot ensure that the argument of this function follows kic-format
