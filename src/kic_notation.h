15ea1d71342b8bf8d6ce14c0e55101fa3c690732
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-02-21 11:00:19 +0900

    chore(modules): rename modules/ -> src/

---
+++ src/kic_notation.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2026 Tomarun029831
+// SPDX-License-Identifier: MIT
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "kic_parser/flyweight_kic_parser.h"
+#include "kic_timestamp/kic_timestamp.h"
+#ifdef __cplusplus
+}
+#endif // KIC_NOTATION_H
